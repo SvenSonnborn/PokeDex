ec47cc66281ea0dd8e63406674a13afc4b180438
Sven Sonnborn <sonnborn.sven@web.de>
2021-12-14 19:26:51 +0100

    added for (multicards), css units, backgroundcolor

--- script.js
+++ script.js
@@ -1,23 +1,44 @@
 let currentPokemon;
+let pokemonnumber = 0;
+let id;
+let classcolor;
 
-async function loadPokemon(){
-    let url = 'https://pokeapi.co/api/v2/pokemon/6';
-    let response = await fetch(url);
-    currentPokemon = await response.json();
-
-    console.log('loaded pokemon', currentPokemon);
-
-    renderPokemonInfo();
-}
-
-function renderPokemonInfo(){
-    document.getElementById('pokemonName').innerHTML = currentPokemon['name'];
-    document.getElementById('pokemonImg').src = currentPokemon['sprites']['other']['official-artwork']['front_default'];
-    document.getElementById('type1').innerHTML = currentPokemon['types'][0]['type']['name'];
-
-    if (currentPokemon['types'].length > 1) {
-        document.getElementById('type2').classList.remove('d-none');
+async function loadPokemon() {
+    for (let i = 0; i < 50; i++) {
+        pokemonnumber++;
+        id = i;
+        let url = `https://pokeapi.co/api/v2/pokemon/${pokemonnumber}`;
+        let response = await fetch(url);
+        currentPokemon = await response.json();
+        document.getElementById('pokedex-complete').innerHTML += `
+        <div id="pokemon-singlecard${id}" class="pokemon-singlecard">
+        <h1 id="pokemonName${id}">Name</h1>
+        <div class="types">
+            <span id="type1${id}" class="type-single">Type1</span>
+            <span id="type2${id}" class="type-single d-none">Type2</span>
+        </div>
+        <img id="pokemonImg${id}" class="pokemonImg">
+    </div>
+        `;
+        console.log('loaded pokemon', currentPokemon);
+        renderPokemonInfo(id);
+        background(id);
     }
-    document.getElementById('type2').innerHTML = currentPokemon['types'][1]['type']['name'];
+}
+function renderPokemonInfo(id) {
+    document.getElementById(`pokemonName${id}`).innerHTML = currentPokemon['name'];
+    document.getElementById(`pokemonImg${id}`).src = currentPokemon['sprites']['other']['official-artwork']['front_default'];
+    document.getElementById(`type1${id}`).innerHTML = currentPokemon['types'][0]['type']['name'];
+    type2(id);
 }
 
+function type2(id){
+    if (currentPokemon['types'].length > 1) {
+        document.getElementById(`type2${id}`).classList.remove('d-none');
+        document.getElementById(`type2${id}`).innerHTML = currentPokemon['types'][1]['type']['name'];
+    }
+}
+function background(id){
+    classcolor = document.getElementById(`type1${id}`).innerHTML;
+    document.getElementById(`pokemon-singlecard${id}`).classList.add(`${classcolor}`);
+}
