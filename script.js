f35465f4f274583210f8c9695ae2ceab4816d5ac
Sven Sonnborn <sonnborn.sven@web.de>
2021-12-14 18:49:15 +0100

    1card complete

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let currentPokemon;
 
 async function loadPokemon(){
-    let url = 'https://pokeapi.co/api/v2/pokemon/4';
+    let url = 'https://pokeapi.co/api/v2/pokemon/6';
     let response = await fetch(url);
     currentPokemon = await response.json();
 
@@ -12,5 +12,12 @@ async function loadPokemon(){
 
 function renderPokemonInfo(){
     document.getElementById('pokemonName').innerHTML = currentPokemon['name'];
-    document.getElementById('pokemonImg').src = currentPokemon['sprites']['front_default'];
+    document.getElementById('pokemonImg').src = currentPokemon['sprites']['other']['official-artwork']['front_default'];
+    document.getElementById('type1').innerHTML = currentPokemon['types'][0]['type']['name'];
+
+    if (currentPokemon['types'].length > 1) {
+        document.getElementById('type2').classList.remove('d-none');
+    }
+    document.getElementById('type2').innerHTML = currentPokemon['types'][1]['type']['name'];
 }
+
